6c5674b95f10d41664224fdd415395714ee83efb
Donna Whisnant <dewhisna@users.noreply.github.com>
2021-06-26 21:44:50 -0500

    Define TString type and consolidate with stringhelp header. Add padString() function to be used with funcanal.

--- src/gendasm/stringhelp.h
+++ src/gendasm/stringhelp.h
@@ -17,53 +17,54 @@
 #include <string_view>
 #include <vector>
 
-typedef std::vector<std::string> CStringArray;
+typedef std::string TString;
+typedef std::vector<TString> CStringArray;
 
 // ============================================================================
 
-static inline bool contains(const CStringArray &arr, const std::string &s)
+static inline bool contains(const CStringArray &arr, const TString &s)
 {
 	return (std::find(arr.cbegin(), arr.cend(), s) != arr.cend());
 }
 
 // trim from start
-static inline std::string &ltrim(std::string &s)
+static inline TString &ltrim(TString &s)
 {
 	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
 	return s;
 }
 
 // trim from end
-static inline std::string &rtrim(std::string &s)
+static inline TString &rtrim(TString &s)
 {
 	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
 	return s;
 }
 
 // trim from both ends
-static inline std::string &trim(std::string &s)
+static inline TString &trim(TString &s)
 {
 	return ltrim(rtrim(s));
 }
 
-static inline std::string &makeUpper(std::string &s)
+static inline TString &makeUpper(TString &s)
 {
 	std::transform(s.begin(), s.end(), s.begin(), ::toupper);
 	return s;
 }
 
-static inline std::string makeUpperCopy(const std::string &s)
+static inline TString makeUpperCopy(const TString &s)
 {
-	std::string strCopy = s;
+	TString strCopy = s;
 	std::transform(strCopy.begin(), strCopy.end(), strCopy.begin(), ::toupper);
 	return strCopy;
 }
 
-static inline int compareNoCase(const std::string &s1, const std::string &s2)
+static inline int compareNoCase(const TString &s1, const TString &s2)
 {
-	std::string strUpper1 = s1;
+	TString strUpper1 = s1;
 	makeUpper(strUpper1);
-	std::string strUpper2 = s2;
+	TString strUpper2 = s2;
 	makeUpper(strUpper2);
 	return ((strUpper1 < strUpper2) ? -1 : ((strUpper1 > strUpper2) ? 1 : 0));
 }
@@ -75,6 +76,15 @@ static bool equals(std::string_view s1, std::string_view s2) {
 
 static bool starts_with(std::string_view s, std::string_view prefix) {
 	return equals(s.substr(0,prefix.size()), prefix);
+}
+
+// padString : Pads a string with spaces up to the specified length.
+static TString padString(const TString &s, TString::size_type nWidth)
+{
+	TString strRetVal = s;
+
+	if (strRetVal.size() < nWidth) strRetVal.append(nWidth-strRetVal.size() , ' ');
+	return strRetVal;
 }
 
 // ============================================================================
