5e4576082dc065d5447116f750ccc13c48115ee7
Donna Whisnant <dewhisna@users.noreply.github.com>
2021-07-04 18:44:16 -0500

    Add 68hc11 helper include files from the m6811dis days.

---
+++ include/cpu/m6811/portse9.asm
@@ -0,0 +1,150 @@
+;
+;  68HC11 Port Equivalency:
+;
+        .module PORTSE9
+        .area   REGSCODE (REL,OVR)
+
+pporta  ==      0x00
+porta:: .blkb   1                       ; $0000 I/O Port A
+; Reserved:
+        .blkb   1                       ; $0001 Reserved
+ppioc	==		0x02
+pioc::	.blkb	1						; $0002 Parallel I/O Control Register
+pportc  ==      0x03
+portc:: .blkb   1                       ; $0003 I/O Port C
+pportb  ==      0x04
+portb:: .blkb   1                       ; $0004 I/O Port B
+pportcl	==		0x05
+portcl::	.blkb	1					; $0005 I/O Port C Latched
+; Reserved:
+        .blkb   1                       ; $0006 Reserved
+pddrc   ==      0x07
+ddrc::  .blkb   1                       ; $0007 Data Direction for C
+pportd  ==      0x08
+portd:: .blkb   1                       ; $0008 I/O Port D
+pddrd   ==      0x09
+ddrd::  .blkb   1                       ; $0009 Data Direction for D
+pporte  ==      0x0A
+porte:: .blkb   1                       ; $000A I/O Port E
+pcforc  ==      0x0B
+cforc:: .blkb   1                       ; $000B Force Output Compare
+poc1m   ==      0x0C
+oc1m::  .blkb   1                       ; $000C OC1 Control Mask
+poc1d   ==      0x0D
+oc1d::  .blkb   1                       ; $000D OC1 Control Data
+ptcnt   ==      0x0E
+ptcnth	==		0x0E
+ptcntl	==		0x0F
+tcnt::									; $000E-000F Timer Counter (Main)
+tcnth::	.blkb	1
+tcntl::	.blkb	1
+ptic1   ==      0x10
+ptic1h	==		0x10
+ptic1l	==		0x11
+tic1::									; $0010-0011 IC1 Timer Counter
+tic1h::	.blkb	1
+tic1l::	.blkb	1
+ptic2   ==      0x12
+ptic2h	==		0x12
+ptic2l	==		0x13
+tic2::									; $0012-0013 IC2 Timer Counter
+tic2h::	.blkb	1
+tic2l::	.blkb	1
+ptic3   ==      0x14
+ptic3h	==		0x14
+ptic3l	==		0x15
+tic3::									; $0014-0015 IC3 Timer Counter
+tic3h::	.blkb	1
+tic3l::	.blkb	1
+ptoc1   ==      0x16
+ptoc1h	==		0x16
+ptoc1l	==		0x17
+toc1::									; $0016-0017 OC1 Timer Counter
+toc1h::	.blkb	1
+toc1l::	.blkb	1
+ptoc2   ==      0x18
+ptoc2h	==		0x18
+ptoc2l	==		0x19
+toc2::									; $0018-0019 OC2 Timer Counter
+toc2h::	.blkb	1
+toc2l::	.blkb	1
+ptoc3   ==      0x1A
+ptoc3h	==		0x1A
+ptoc3l	==		0x1B
+toc3::									; $001A-001B OC3 Timer Counter
+toc3h::	.blkb	1
+toc3l::	.blkb	1
+ptoc4   ==      0x1C
+ptoc4h	==		0x1C
+ptoc4l	==		0x1D
+toc4::									; $001C-001D OC4 Timer Counter
+toc4h::	.blkb	1
+toc4l::	.blkb	1
+pti4o5  ==      0x1E
+pti4o5h	==		0x1E
+pti4o5l	==		0x1F
+ti4o5::									; $001E-001F IC4 & OC5 Timer Counter
+ti4o5h::	.blkb	1
+ti4o5l::	.blkb	1
+ptctl1  ==      0x20
+tctl1:: .blkb   1                       ; $0020 Timer Control 1
+ptctl2  ==      0x21
+tctl2:: .blkb   1                       ; $0021 Timer Control 2
+ptmsk1  ==      0x22
+tmsk1:: .blkb   1                       ; $0022 Timer Mask 1
+ptflg1  ==      0x23
+tflg1:: .blkb   1                       ; $0023 Timer Flag 1
+ptmsk2  ==      0x24
+tmsk2:: .blkb   1                       ; $0024 Timer Mask 2
+ptflg2  ==      0x25
+tflg2:: .blkb   1                       ; $0025 Timer Flag 2
+ppactl  ==      0x26
+pactl:: .blkb   1                       ; $0026 Pulse Accum Control
+ppacnt  ==      0x27
+pacnt:: .blkb   1                       ; $0027 Pulse Accum Count
+pspcr   ==      0x28
+spcr::  .blkb   1                       ; $0028 SPI Control Register
+pspsr   ==      0x29
+spsr::  .blkb   1                       ; $0029 SPI Status Register
+pspdr   ==      0x2A
+spdr::  .blkb   1                       ; $002A SPI Data Register
+pbaud   ==      0x2B
+baud::  .blkb   1                       ; $002B Baud Set Register
+psccr1  ==      0x2C
+sccr1:: .blkb   1                       ; $002C SCI Control Register 1
+psccr2  ==      0x2D
+sccr2:: .blkb   1                       ; $002D SCI Control Register 2
+pscsr   ==      0x2E
+scsr::  .blkb   1                       ; $002E SCI Status Register
+pscdr   ==      0x2F
+scdr::  .blkb   1                       ; $002F SCI Data Register
+padctl  ==      0x30
+adctl:: .blkb   1                       ; $0030 A/D Conv Control Reg
+padr1   ==      0x31
+adr1::  .blkb   1                       ; $0031 A/D Result Reg 1
+padr2   ==      0x32
+adr2::  .blkb   1                       ; $0032 A/D Result Reg 2
+padr3   ==      0x33
+adr3::  .blkb   1                       ; $0033 A/D Result Reg 3
+padr4   ==      0x34
+adr4::  .blkb   1                       ; $0034 A/D Result Reg 4
+pbprot  ==      0x35
+bprot:: .blkb   1                       ; $0035 Protection Control Reg
+peprog	==		0x36
+eprog::	.blkb	1						; $0036 EPROM Programming Control (MC68HC711E20 only)
+; Reserved:
+        .blkb   2                       ; $0037-0038 Reserved
+poption ==      0x39
+option:: .blkb  1                       ; $0039 Option Reg
+pcoprst ==      0x3A
+coprst:: .blkb  1                       ; $003A COP Watch Dog Arm/Reset
+ppprog  ==      0x3B
+pprog:: .blkb   1                       ; $003B EEProm Prog Ctrl
+phprio  ==      0x3C
+hprio:: .blkb   1                       ; $003C Hi-Priority Set Reg
+pinit   ==      0x3D
+init::  .blkb   1                       ; $003D Init Reg
+ptest1  ==      0x3E
+test1:: .blkb   1                       ; $003E Test Ctrl Reg
+pconfig ==      0x3F
+config:: .blkb  1                       ; $003F Configuration Reg
---
+++ include/cpu/m6811/portsf1.h
@@ -0,0 +1,41 @@
+;
+;  68HC11 Port Equivalency:
+;
+
+; Offset Pointers to Regs:
+        .globl  pporta,pddra,pportg,pddrg
+        .globl  pportb,pportf,pportc,pddrc
+        .globl  pportd,pddrd,pporte,pcforc
+        .globl  poc1m,poc1d,ptcnt,ptcnth,ptcntl
+        .globl  ptic1,ptic1h,ptic1l,ptic2,ptic2h,ptic2l
+		.globl	ptic3,ptic3h,ptic3l,ptoc1,ptoc1h,ptoc1l
+        .globl  ptoc2,ptoc2h,ptoc2l,ptoc3,ptoc3h,ptoc3l
+		.globl	ptoc4,ptoc4h,ptoc4l,pti4o5,pti4o5h,pti4o5l
+        .globl  ptctl1,ptctl2,ptmsk1,ptflg1
+        .globl  ptmsk2,ptflg2,ppactl,ppacnt
+        .globl  pspcr,pspsr,pspdr,pbaud
+        .globl  psccr1,psccr2,pscsr,pscdr
+        .globl  padctl,padr1,padr2,padr3
+        .globl  padr4,pbprot
+        .globl  popt2,poption,pcoprst,ppprog
+        .globl  phprio,pinit,ptest1,pconfig
+        .globl  pcsstrh,pcsctl,pcsgadr,pcsgsiz
+
+; Direct Pointers to Regs:
+        .globl  porta,ddra,portg,ddrg
+        .globl  portb,portf,portc,ddrc
+        .globl  portd,ddrd,porte,cforc
+        .globl  oc1m,oc1d,tcnt,tcnth,tcntl
+        .globl  tic1,tic1h,tic1l,tic2,tic2h,tic2l
+		.globl	tic3,tic3h,tic3l,toc1,toc1h,toc1l
+        .globl  toc2,toc2h,toc2l,toc3,toc3h,toc3l
+		.globl	toc4,toc4h,toc4l,ti4o5,ti4o5h,ti4o5l
+        .globl  tctl1,tctl2,tmsk1,tflg1
+        .globl  tmsk2,tflg2,pactl,pacnt
+        .globl  spcr,spsr,spdr,baud
+        .globl  sccr1,sccr2,scsr,scdr
+        .globl  adctl,adr1,adr2,adr3
+        .globl  adr4,bprot
+        .globl  opt2,option,coprst,pprog
+        .globl  hprio,init,test1,config
+        .globl  csstrh,csctl,csgadr,csgsiz
